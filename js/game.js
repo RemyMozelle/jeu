72c923fcb9cd17bf573191623b8de984a0b07f61
RemyMozelle <timoz974@hotmail.fr>
2017-09-10 17:23:51 +0400

    principal fonctionnalité Opé

--- js/game.js
+++ js/game.js
@@ -2,28 +2,27 @@ import { Personnage } from './Personnage.js'
 import { Hero } from './Hero.js'
 import { Minion } from './Minion.js'
 
-let denilson = new Hero('denilson', 200, 25, 100)
-let minion = new Minion('minion', 300, 25)
+let denilson = new Hero('denilson', 200, 25, 0)
+let minion = new Minion('minion', 500, 25)
 
+let name = document.querySelectorAll('h4')
+let life = document.querySelectorAll('.badge-success')
+let dammage = document.querySelectorAll('.badge-danger')
+let armor = document.querySelector('.badge-primary')
+let attack = document.querySelector('button')
 
-while (denilson.survive() === false && denilson.survive() === false) {
+name[0].innerHTML = denilson.name
+name[1].innerHTML = minion.name
 
-    let gainHp = Math.round(Math.random() * 5)
-    let gainArmor = Math.round(Math.random() * 3)
+attack.addEventListener('click', (e) => {
+    denilson.attack(minion);
 
-    if(denilson.hp > 0){
-        denilson.attack(minion)
-    }
+    life[0].innerHTML = denilson.hp
+    life[1].innerHTML = minion.hp
 
-    if(minion.hp > 0){
-        minion.attack(denilson)
-    }
-    
-    if (minion.hp <= 0 || denilson.hp <= 0) {
-        break
-    }
+    dammage[0].innerHTML = denilson.dammage
+    dammage[1].innerHTML = minion.dammage
 
-    console.log(`
-    {%---------- % NOUVEAU ROUND % ----------%}
-    `)
-}
+    armor.innerHTML = denilson.armor
+    console.log(e);
+})
